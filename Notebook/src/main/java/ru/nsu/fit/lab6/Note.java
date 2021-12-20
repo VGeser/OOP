05675abe41158131996dc5c2c618669bb821ff3a
VGeser <50826449+VGeser@users.noreply.github.com>
2021-12-20 17:09:32 +0700

    One good commit

---
+++ Notebook/src/main/java/ru/nsu/fit/lab6/Note.java
@@ -0,0 +1,29 @@
+package ru.nsu.fit.lab6;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Note {
+    @SerializedName("timestamp")
+    private final String timestamp;
+
+    @SerializedName("text")
+    private final String text;
+
+    public Note(String timestamp, String text) {
+        this.timestamp = timestamp;
+        this.text = text;
+    }
+
+    /**
+     *there are no setters because no editing is required
+     */
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+}
