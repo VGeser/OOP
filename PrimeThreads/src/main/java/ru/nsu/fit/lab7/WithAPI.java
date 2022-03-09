84d34b925cdb4ca114be95fd2ade3938687ad58f
VGeser <50826449+VGeser@users.noreply.github.com>
2022-03-09 22:06:41 +0700

    Truly better tests

--- PrimeThreads/src/main/java/ru/nsu/fit/lab7/WithAPI.java
+++ PrimeThreads/src/main/java/ru/nsu/fit/lab7/WithAPI.java
@@ -1,15 +1,12 @@
 package ru.nsu.fit.lab7;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class WithAPI {
     public boolean apiPrime(int[] arr) {
         Decider decider = Decider.getInstance();
-        List<Integer> arrListed = new ArrayList<>(arr.length);
-        for (int i : arr) {
-            arrListed.add(i);
-        }
-        return arrListed.parallelStream().anyMatch(decider::isNotPrime);
+        IntStream stream = Arrays.stream(arr);
+        return stream.parallel().anyMatch(decider::isNotPrime);
     }
 }
