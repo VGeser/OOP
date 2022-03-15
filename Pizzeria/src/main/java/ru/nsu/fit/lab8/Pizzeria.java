afbd4f7024d1446fb7118d6e2b9991c44036f5f2
VGeser <50826449+VGeser@users.noreply.github.com>
2022-03-16 02:15:50 +0700

    Work in progress

--- Pizzeria/src/main/java/ru/nsu/fit/lab8/Pizzeria.java
+++ Pizzeria/src/main/java/ru/nsu/fit/lab8/Pizzeria.java
@@ -6,19 +6,27 @@ import java.util.Queue;
 import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 
 import static java.lang.Thread.sleep;
 
 public class Pizzeria {
+    //constructing parameters
     private final int chefNum;
     private final int delivNum;
-    private int capacity;
+    private final int capacity;
+
+    //shared data structures
     private int busy = 0;
-    int[] warehouse;
-    private int orderID = 0;
+    private int[] warehouse;
+    private Integer orderID = 0;
     private Queue<Integer> orderQueue;
-    Random random = new Random();
+
+    //required access from subclasses and big class
+    private final Random random = new Random();
+    private Chef[] chefContainer;
+    private Delivery[] deliveryContainer;
+    private ExecutorService chefExecutor;
+    private ExecutorService deliveryExecutor;
 
     public Pizzeria(int capacity, int chefNum, int delivNum) {
         this.chefNum = chefNum;
@@ -27,30 +35,43 @@ public class Pizzeria {
         this.warehouse = new int[capacity];
         Arrays.fill(warehouse, -1);
         orderQueue = new LinkedList<>();
+        this.chefExecutor = Executors.newFixedThreadPool(chefNum);
+        this.deliveryExecutor = Executors.newFixedThreadPool(delivNum);
     }
 
     public void placeOrder() {
-        orderQueue.add(orderID);
+        synchronized (orderQueue) {
+            orderQueue.add(orderID);
+            notify();
+        }
         System.out.println("Your order number is " + orderID);
-        orderID++;
+        synchronized (orderID) {
+            orderID++;
+        }
     }
 
     public void startPizzeria() {
-        ExecutorService pool = Executors.newFixedThreadPool(chefNum+delivNum+1);
-        //что делает диспетчер ? как он что он куда
+        chefContainer = new Chef[chefNum];
         for (int i = 0; i < chefNum; i++) {
-            Chef cur = new Chef(random.nextInt(10));
-            placeOrder();
-            cur.setCurrentOrder(orderID);
-            pool.execute(cur);
+            Chef cur = new Chef();
+            cur.setExperience(random.nextInt(50));
+            cur.hasOrder = false;
+            chefContainer[i] = cur;
         }
+        deliveryContainer = new Delivery[delivNum];
         for (int i = 0; i < delivNum; i++) {
-            Runnable cur = new Delivery(random.nextInt(50));
-            pool.execute(cur);
+            Delivery cur = new Delivery();
+            cur.setBaggageCapacity(random.nextInt(5));
+            cur.hasOrder = false;
+            deliveryContainer[i] = cur;
         }
-        //int currentOrder = orderQueue.remove();
-        //TODO: диспетчер сам берет из очереди и отдает кому надо
-
+        Manager manager = new Manager();
+        //TODO: change conditions!!!!!
+        while(true){
+            manager.run();
+        }
+        //manager.interrupt();
+       /*
         pool.shutdown();
         try {
             if (!pool.awaitTermination(800, TimeUnit.MILLISECONDS)) {
@@ -59,31 +80,58 @@ public class Pizzeria {
         } catch (InterruptedException e) {
             pool.shutdownNow();
         }
+        *
+        */
     }
 
-    class Manager implements Runnable{
+    class Manager implements Runnable {
+
+        //TODO: adequate first free task finder
+        //if no free return -1
+        private int firstFree(Runnable[] array) {
+            return 0;
+        }
 
         @Override
         public void run() {
-
+            if (orderQueue.isEmpty()) {
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                int currentOrder = orderQueue.remove();
+                int freeChef = firstFree(chefContainer);
+                //completable future????
+                //i cant wait on a runnable container,
+                //but i need to know if and when a chef is free
+                chefContainer[freeChef].setCurrentOrder(currentOrder);
+                chefExecutor.execute(chefContainer[freeChef]);
+                int freeCourier= firstFree(deliveryContainer);
+                deliveryExecutor.execute(deliveryContainer[freeCourier]);
+            }
         }
     }
 
-    //producer
+
     class Chef implements Runnable {
-        private final int experience;
+        public void setExperience(int experience) {
+            this.experience = experience;
+        }
+
+        private int experience;
+        public boolean hasOrder;
 
         public void setCurrentOrder(int currentOrder) {
             this.currentOrder = currentOrder;
+            hasOrder = true;
         }
 
         private int currentOrder;
 
-        public Chef(int exp) {
-            this.experience = exp;
-        }
-
-        private int nextFreeOrder() {
+        //shuffle?
+        private int nextFreeRoom() {
             for (int i = 0; i < capacity; i++) {
                 if (warehouse[i] == -1)
                     return i;
@@ -102,10 +150,12 @@ public class Pizzeria {
             System.out.println("Order " + currentOrder + " cooked");
             synchronized (warehouse) {
                 if (busy < capacity) {
-                    int next = nextFreeOrder();
+                    int next = nextFreeRoom();
                     warehouse[next] = currentOrder;
                     notify();
                     busy++;
+                    System.out.println("Order " + currentOrder + " put to warehouse");
+                    hasOrder = false;
                 } else {
                     try {
                         wait();
@@ -117,22 +167,23 @@ public class Pizzeria {
         }
     }
 
-    //consumer
     class Delivery implements Runnable {
-        private final int baggageCapacity;
+        public void setBaggageCapacity(int baggageCapacity) {
+            this.baggageCapacity = baggageCapacity;
+            this.baggage = new int[baggageCapacity];
+        }
+
+        public boolean hasOrder;
+        private int baggageCapacity;
         private int[] baggage;
         private int taken = 0;
 
-        public Delivery(int cap) {
-            this.baggage = new int[cap];
-            this.baggageCapacity = cap;
-        }
-
         private void takeOrders() {
+            hasOrder = true;
             int i = 0;
             while (taken < baggageCapacity || i < capacity) {
                 if (warehouse[i] != -1) {
-                    System.out.println("Order "+warehouse[i]+" taken to delivery");
+                    System.out.println("Order " + warehouse[i] + " taken to delivery");
                     baggage[taken] = warehouse[i];
                     taken++;
                     busy--;
@@ -148,17 +199,18 @@ public class Pizzeria {
                 if (busy > 0) {
                     takeOrders();
                     notify();
-                    int temp =taken;
-                    for (int i=0;i<temp;i++) {
+                    int temp = taken;
+                    for (int i = 0; i < temp; i++) {
                         try {
                             sleep(random.nextInt(20));
-                            System.out.println("Order "+baggage[i]+" delivered");
+                            System.out.println("Order " + baggage[i] + " delivered");
                             taken--;
-                            baggage[i]=0;
+                            baggage[i] = 0;
                         } catch (InterruptedException e) {
                             e.printStackTrace();
                         }
                     }
+                    hasOrder = false;
                 } else {
                     try {
                         wait();
