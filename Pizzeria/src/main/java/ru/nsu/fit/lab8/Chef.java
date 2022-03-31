7c3e314a6c8fe43fa4101fe17e646b442c68ddb1
VGeser <vgeser.13@ya.ru>
2022-03-31 21:28:35 +0700

    SOLID + debugging

--- Pizzeria/src/main/java/ru/nsu/fit/lab8/Chef.java
+++ Pizzeria/src/main/java/ru/nsu/fit/lab8/Chef.java
@@ -1,53 +1,38 @@
 package ru.nsu.fit.lab8;
 
 import static java.lang.Thread.sleep;
-import static ru.nsu.fit.lab8.PizzeriaDispatcher.*;
 
 public class Chef implements Runnable {
     private final int experience;
     private int currentOrder;
+    private final Containers containers;
+    boolean stop;
 
-    public Chef(int experience, int order) {
+    public Chef(int experience, int order, Containers containers) {
         this.experience = experience;
         this.currentOrder = order;
-    }
-
-    private int nextFreeRoom() {
-        int size = warehouseCapacity;
-        synchronized (warehouse) {
-            for (int i = 0; i < size; i++) {
-                if (warehouse[i] == -1)
-                    return i;
-            }
-        }
-        return -1;
+        this.containers = containers;
+        stop = false;
     }
 
     @Override
     public void run() {
-        System.out.println("Cooking order " + currentOrder);
-        try {
-            sleep(experience);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        System.out.println("Order " + currentOrder + " cooked");
-
-        synchronized(warehouse){
-            try{
-                while(!(busy < warehouseCapacity)){
-                    wait();
-                }
-                int next = nextFreeRoom();
-                warehouse[next] = currentOrder;
-                notify();
-                busy++;
-                System.out.println("Order " + currentOrder + " put to warehouse");
-            }catch(InterruptedException e){
+        while (!stop) {
+            System.out.println("Cooking order " + currentOrder + Thread.currentThread().getName());
+            try {
+                sleep(experience);
+            } catch (InterruptedException e) {
                 e.printStackTrace();
+            } finally {
+                System.out.println("Order " + currentOrder + " cooked");
+                containers.putToWarehouse(currentOrder);
+                try {
+                    currentOrder = containers.getOrder();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
         }
-        //currentOrder = PizzeriaDispatcher.getOrder();
     }
 
 }
